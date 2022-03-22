13ea2298c928b6098f6c6aac01f121ddbe1d3686
Aaron <andy1213aa@gmail.com>
2022-03-22 15:59:18 +0800

    Init

---
+++ Encoder.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+from tensorflow.keras import layers, initializers
+class encoder():
+
+    def __init__():
+
+        F1 = layers.Dense()
---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+Implement "Multi-Task GANs for View-Specific Feature Learning in Gait Recognition"
+
+'''
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
